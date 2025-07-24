6b1d573597290656f9aca3cbaf2cdaf47e84f9c6
Simrankaursaini <sainisimran1227@gmail.com>
2025-07-24 16:24:52 +0530

    Add files via upload

---
+++ pratical3.py
@@ -0,0 +1,9 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+c = int(input("enter third number:"))
+if a>b and a>c:
+    print("first number is greater")
+elif a<b and b>c:
+    print("second number is greater")
+else :
+    print("third number is greater")
